a1302c021a1603cf1b56372d19419d8ed1adf42f
amey.sunu13 <amey.sunu13@gmail.com>
2024-07-09 10:44:34 +0000

    sql removed

--- advena_flutter/lib/controllers/recommendation.dart
+++ advena_flutter/lib/controllers/recommendation.dart
@@ -1,41 +1,5 @@
-import 'package:advena_flutter/controllers/sqlcontroller.dart';
-
 class RecommendationController {
-  SqlController _sqlController = SqlController();
-
   Future<bool> onLoadRecommendation() async {
-    try {
-      var isTableExists = await createRecommendationIfNotExists();
-      if (isTableExists) {
-        List<Map<String, dynamic>> items =
-            await _sqlController.getItems("recommendation");
-        print("ITEMS: $items");
-        if (items.isEmpty) {
-          print("Empty Recommendations");
-          return false;
-        } else {
-          return true;
-        }
-      }
-
-      return false;
-    } catch (e) {
-      print(e);
-      return false;
-    }
-  }
-
-  Future<bool> createRecommendationIfNotExists() async {
-    bool tableExists = await _sqlController.isTableExists("recommendation");
-    if (!tableExists) {
-      var db = await _sqlController.database;
-      var createQuery =
-          "CREATE TABLE recommendation (id INTEGER PRIMARY KEY AUTOINCREMENT, type TEXT NOT NULL, event TEXT NOT NULL)";
-      await _sqlController.createTable(db, createQuery);
-
-      return false;
-    }
-
-    return true;
+    return false;
   }
 }
--- advena_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ advena_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,6 @@ import firebase_core
 import firebase_storage
 import google_sign_in_ios
 import location
-import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -23,6 +22,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
