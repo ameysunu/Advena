44f1b945320c46ae45851988de6537595e6c1e42
ameysunu <ameysunu00719@gmail.com>
2024-07-28 11:13:17 +0100

    gemini call added to the function app

--- backend/AdvenaBackend/Model.cs
+++ backend/AdvenaBackend/Model.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AdvenaBackend
 {
-    class RecommendationPayloadData
+    public class RecommendationPayloadData
     {
        public String userId {  get; set; }
        public String userLocation { get; set; }
@@ -14,10 +14,40 @@ namespace AdvenaBackend
        public SocialPreferences socialPreferences {  get; set; }
     }
 
-    class SocialPreferences
+    public class SocialPreferences
     {
         public String dining { get; set; }
         public int groupSize { get; set; }
         public String socializing { get; set;}
+    }
+
+    public class Response
+    {
+        public List<Candidate> candidates { get; set; }
+    }
+
+    public class Content
+    {
+        public List<Part> parts { get; set; }
+        public string role { get; set; }
+    }
+
+    public class Candidate
+    {
+        public Content content { get; set; }
+        public string finishReason { get; set; }
+        public int index { get; set; }
+        public List<SafetyRating> safetyRatings { get; set; }
+    }
+
+    public class Part
+    {
+        public string text { get; set; }
+    }
+
+    public class SafetyRating
+    {
+        public string category { get; set; }
+        public string probability { get; set; }
     }
 }
--- backend/AdvenaBackend/RecommendationEngine.cs
+++ backend/AdvenaBackend/RecommendationEngine.cs
@@ -7,6 +7,9 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Text;
 
 namespace AdvenaBackend
 {
@@ -28,7 +31,96 @@ namespace AdvenaBackend
                 return new BadRequestObjectResult(new { error = "Payload cannot be null" });
             }
 
+            String interestsJsonPayload = QueryPreparerForGemini(log, data, true);
+            String geminiInterests = await GetDataFromGemini(log, interestsJsonPayload);
+
+            if (geminiInterests.Contains("Error"))
+            {
+                return new BadRequestObjectResult(new { error = geminiInterests });
+            }
+
             return new OkObjectResult($"Welcome to Azure Functions {data.userId}");
+        }
+
+        public static String QueryPreparerForGemini(ILogger logger, RecommendationPayloadData data, bool isInterests)
+        {
+            String userTextContent = "";
+            String country = data.userLocation;
+
+            if (isInterests)
+            {
+
+                logger.LogInformation("Query type is of interests");
+
+                String interests = String.Join(",", data.interests);
+                userTextContent = $"Suggest me top 10 places in {country} for my interests: {interests}";
+            } else
+            {
+                logger.LogInformation("Query type is of social preferences");
+
+                String dining = data.socialPreferences.dining;
+                String groupSize = data.socialPreferences.groupSize.ToString();
+                String socializing = data.socialPreferences.socializing;
+
+                userTextContent = $"Suggest me top 10 places in {country} that incorporate my social preferences as - dining: {dining}, my group size: {groupSize} and my socializing type of being {socializing}";
+            }
+
+            return $@"
+        {{
+            ""contents"": [
+                {{
+                    ""parts"": [
+                        {{
+                            ""text"": ""{userTextContent}""
+                        }}
+                    ]
+                }}
+            ]
+        }}";
+        }
+        
+        public static async Task<String> GetDataFromGemini(ILogger log, String payload)
+        {
+            log.LogInformation("Preparing to send data to Gemini");
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            String geminiEndPoint = configuration["GEMINI_ENDPOINT"];
+            String geminiApiKey = configuration["GEMINI_API_KEY"];
+
+            String Url = geminiEndPoint + "/gemini-1.5-flash-latest:generateContent?key=" + geminiApiKey;
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, Url);
+                request.Content = new StringContent(payload, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.SendAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    var responseObj = JsonConvert.DeserializeObject<Response>(responseBody);
+
+                    if (responseObj?.candidates != null && responseObj.candidates.Count > 0)
+                    {
+                        var text = responseObj.candidates[0].content.parts[0].text;
+                        log.LogInformation(text);
+                        return text;
+                    }
+
+                    return responseBody;
+                }
+                else
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    log.LogInformation($"Failed to call the API. Status code: {response.StatusCode}");
+                    return "Error: " + responseBody;
+                }
+            }
         }
 
     }
